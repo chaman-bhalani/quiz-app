be2abdc418cce0a5314db8cee33d9b96deab4ff8
chaman_bhalani <108402674+chaman-bhalani@users.noreply.github.com>
2025-04-10 16:43:34 +0530

    Initial commit

---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import { resultInitialState } from "../../constants";
+import "./Quiz.scss";
+import AnswerTimer from "../AnswerTimer/AnswerTimer.jsx";
+import Result from "../Result/Result.jsx";
+
+const Quiz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answerIdx, setAnswerIdx] = useState(null);
+  const [answer, setAnswer] = useState(null);
+  const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
+  const [showAnswerTimer, setShowAnswerTimer] = useState(true);
+  const [inputAnswer, setInputAnswer] = useState("");
+
+  const { question, choices, correctAnswer, type } = questions[currentQuestion];
+
+  const onAnswerClick = (answer, index) => {
+    setAnswerIdx(index);
+    if (answer === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  };
+
+  const onClickNext = (finalAnswer) => {
+    setAnswerIdx(null);
+    setShowAnswerTimer(false);
+
+    setResult((prev) =>
+      finalAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
+    );
+    if (currentQuestion !== questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setCurrentQuestion(0);
+      setShowResult(true);
+    }
+
+    setInputAnswer('');
+
+    setTimeout(() => {
+      setShowAnswerTimer(true);
+    }, 100);
+  };
+
+  const onTryAgain = () => {
+    setResult(resultInitialState);
+    setShowResult(false);
+  };
+
+  const handleTimeUp = () => {
+    setAnswer(false);
+    onClickNext(false);
+  };
+
+  const handleInputChange = (evt) => {
+    setInputAnswer(evt.target.value);
+
+    if (evt.target.value === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  };
+
+  const getanswerUi = () => {
+    if (type === "FIB") {
+      return <input value={inputAnswer} onChange={handleInputChange} />;
+    }
+
+    return (
+      <ul>
+        {choices.map((choice, index) => (
+          <li
+            onClick={() => onAnswerClick(choice, index)}
+            key={choice}
+            className={answerIdx === index ? "selected-answer" : null}
+          >
+            {choice}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div className="quiz-container">
+      {!showResult ? (
+        <>
+          {showAnswerTimer && (
+            <AnswerTimer duration={5} onTimeUp={handleTimeUp} />
+          )}
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-questions">/{questions.length}</span>
+          <h2>{question}</h2>
+          {getanswerUi()}
+
+          <div className="footer">
+            <button
+              onClick={() => onClickNext(answer)}
+              disabled={answerIdx === null && inputAnswer.trim() === ""}
+            >
+              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <Result
+          result={result}
+          onTryAgain={onTryAgain}
+          totalQuestions={questions.length}
+        />
+      )}
+    </div>
+  );
+};
+export default Quiz;
